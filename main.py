7d2be4211041f6267f25e523619b98595b3be943
Baturevich Dmitry <81748626+Freek0@users.noreply.github.com>
2023-04-20 13:41:33 +0300

    HW 16 (#4)
    * HW 16
    
    * Fix HW16
    
    * Use BLACK
    
    ---------
    
    Co-authored-by: freeko <freeko@t-t.test.test>

--- main.py
+++ main.py
@@ -1,6 +1,68 @@
-import os
+import math
 
-if os.environ.get('SHELL') == '/bin/bash':
-    print('Greetings bash')
-else:
-    print(f'Hello {os.environ.get("SHELL")}')
+# Входные данные
+sum_arr = [
+    "1_1000",
+    "2_30000",
+    "3_100000",
+    "8_100",
+    "5_11111",
+    "9_14124124124",
+    "6_444",
+    "4_123456",
+    "7_100000000000",
+    "10_81214",
+]
+rate_arr = [
+    "1_10",
+    "2_11",
+    "3_8",
+    "4_13",
+    "5_11",
+    "6_6",
+    "7_9",
+    "8_11",
+    "9_13",
+    "10_12",
+]
+term_arr = ["1_1", "2_2", "3_2", "4_6", "5_8", "6_20", "7_9", "8_11", "9_13", "10_12"]
+
+# Сортируем массивы по id
+sum_arr = sorted(sum_arr, key=lambda x: int(x.split("_")[0]))
+rate_arr = sorted(rate_arr, key=lambda x: int(x.split("_")[0]))
+term_arr = sorted(term_arr, key=lambda x: int(x.split("_")[0]))
+
+
+# Функция для расчета итоговой суммы по каждой строке
+def calculate_end_sum(start_sum, rate, term):
+    r = rate / 100
+    n = term
+    return start_sum * (1 + r) ** n
+
+
+# Создаем список словарей с данными по каждой строке
+result_arr = []
+for i in range(len(sum_arr)):
+    id, start_sum = sum_arr[i].split("_")
+    id, rate = rate_arr[i].split("_")
+    id, term = term_arr[i].split("_")
+    id = int(id)
+    start_sum = int(start_sum)
+    rate = float(rate)
+    term = int(term)
+    end_sum = calculate_end_sum(start_sum, rate, term)
+    result_arr.append(
+        {
+            "id": id,
+            "start_sum": start_sum,
+            "rate": rate,
+            "term": term,
+            "end_sum": math.ceil(end_sum),
+        }
+    )
+
+# Результат
+for item in result_arr:
+    print(
+        f"id: {item['id']}\nstart_sum: {item['start_sum']}\nrate: {item['rate']}\nterm: {item['term']}\nend_sum: {item['end_sum']}\n"
+    )
