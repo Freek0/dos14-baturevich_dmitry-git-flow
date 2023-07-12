668c95a22980ca2d758912fca4a410998db255d2
freeko <freeko@t-t.test.test>
2023-07-12 15:44:33 +0300

    add dockerfile.certbot & entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+certs_dir="/etc/letsencrypt/live/bdi.bank.smodata.net"
+domains="bdi.bank.smodata.net"
+
+if [ -d "$certs_dir" ]; then
+    echo "Сертификаты уже существуют"
+    if certbot certificates --domain $domains --quiet; then
+        echo "Сертификаты актуальны"
+    else
+        echo "Сертификаты устарели"
+        certbot renew --quiet
+    fi
+else
+    echo "Сертификаты отсутствуют"
+    certbot certonly --webroot --webroot-path=/var/www/certbot --domain $domains --agree-tos --non-interactive --email baturevichdmitry@gmail.com --quiet
+fi
+exec "$@"
